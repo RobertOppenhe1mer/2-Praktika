c8c5a6e4a2320772dc4399668335f77607934d2b
RobertOppenhe1mer <artemagishew16@gmail.com>
2024-06-01 10:17:33 +0300

    Вторая часть

---
+++ app/src/main/java/ru/rsue/agyshew/quest/Book.java
@@ -0,0 +1,24 @@
+package ru.rsue.agyshew.quest;
+
+import java.util.UUID;
+
+public class Book {
+    public UUID getId() {
+        return mId;
+    }
+
+    private UUID mId;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    private String mTitle;
+    public Book() {
+        mId = UUID.randomUUID(); //Генерирование уникального идентификатора
+    }
+}
---
+++ app/src/main/java/ru/rsue/agyshew/quest/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.rsue.agyshew.quest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null){
+            fragment = new BookFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+    }
+}}
